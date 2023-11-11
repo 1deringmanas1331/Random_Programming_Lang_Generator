6905d3e39b2d19016d80f91f04d73f4ed02fcc11
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-11 08:18:09 +0530

    Random_Prog_Lang_generator.py

--- Random_Prog_Lang_generator.py
+++ Random_Prog_Lang_generator.py
@@ -1,12 +1,12 @@
-import random
+ import random
 
 # List of Programming Languages
 lang = ["Python", "JavaScript", "Java", "C++", "C#", "Ruby", "Swift", "Go", "PHP", "Rust", "Kotlin", "TypeScript", "Scala", "Perl", "Haskell", "Lua", "Dart", "COBOL", "Fortran", "Ada", "Lisp", "Prolog", "Assembly", "R", "MATLAB", "Objective-C", "VHDL", "Elixir", "Clojure", "Pascal", "F#", "Groovy", "Erlang", "Julia", "LabVIEW", "Scheme", "Shell", "SQL", "Verilog", "Visual Basic", "Forth", "HTML", "SQL","C"
    
 ]
 
-#selects random programming language from the given list   
+# Selects Random Programming Language from the given list   
 random_lang = random.choice(lang)
 
-
+# Prints the programming language choosen
 print("Random Programming Language:", random_lang)
