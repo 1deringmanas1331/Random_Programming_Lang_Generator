460af7f839ef9ec1582b60b37a06542b9072234e
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-11 08:16:00 +0530

    Update Random_Prog_Lang_generator.py

--- Random_Prog_Lang_generator.py
+++ Random_Prog_Lang_generator.py
@@ -1,6 +1,6 @@
 import random
 
-# List of Languages names
+# List of Programming Languages
 lang = ["Python", "JavaScript", "Java", "C++", "C#", "Ruby", "Swift", "Go", "PHP", "Rust", "Kotlin", "TypeScript", "Scala", "Perl", "Haskell", "Lua", "Dart", "COBOL", "Fortran", "Ada", "Lisp", "Prolog", "Assembly", "R", "MATLAB", "Objective-C", "VHDL", "Elixir", "Clojure", "Pascal", "F#", "Groovy", "Erlang", "Julia", "LabVIEW", "Scheme", "Shell", "SQL", "Verilog", "Visual Basic", "Forth", "HTML", "SQL","C"
    
 ]
